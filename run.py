11751e06ad867d123ef83503ed1fc5df83932774
vn.py <xiaoyou.chen@foxmail.com>
2022-08-21 20:53:06 +0800

    [Mod] 第二段：使用PySide6图形界面来显示信息输出

--- run.py
+++ run.py
@@ -1,16 +1,20 @@
+from PySide6 import QtWidgets
+
 from vnpy_ctp.api import MdApi
 
 
 class CtpMdApi(MdApi):
     """实现行情API"""
 
-    def __init__(self):
+    def __init__(self, log_monitor):
         """"""
         super().__init__()
 
+        self.log_monitor = log_monitor
+
     def onFrontConnected(self):
         """服务器连接成功回报"""
-        print("行情服务器连接成功")
+        self.write_log("行情服务器连接成功")
 
         # 发起登录操作
         ctp_req: dict = {
@@ -22,27 +26,38 @@ class CtpMdApi(MdApi):
 
     def onFrontDisconnected(self, reason):
         """服务器连接断开回报"""
-        print("行情服务器连接断开", reason)
+        self.write_log(f"行情服务器连接断开{reason}")
 
     def onRspUserLogin(self, data, error, reqid, last):
         """用户登录请求回报"""
         if not error["ErrorID"]:
-            print("行情服务器登录成功")
+            self.write_log("行情服务器登录成功")
 
             # 订阅行情推送
             self.subscribeMarketData("rb2301")
         else:
-            print("行情服务器登录失败", error)
+            self.write_log(f"行情服务器登录失败{error}")
 
     def onRtnDepthMarketData(self, data):
         """行情数据推送回调"""
-        print(data)
+        self.write_log(str(data))
+
+    def write_log(self, msg):
+        """"""
+        self.log_monitor.append(msg)
 
 
 def main():
     """主函数"""
+    # 创建Qt应用
+    app = QtWidgets.QApplication()
+
+    # 创建日志监控组件
+    log_monitor = QtWidgets.QTextEdit()
+    log_monitor.show()
+
     # 创建实例
-    api = CtpMdApi()
+    api = CtpMdApi(log_monitor)
 
     # 初始化底层
     api.createFtdcMdApi(".")
@@ -53,8 +68,8 @@ def main():
     # 发起连接
     api.init()
 
-    # 阻塞主线程推出
-    input()
+    # 启动主线程UI循环
+    app.exec()
 
 
 if __name__ == "__main__":
