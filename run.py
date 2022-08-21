950c6a91b6f8d8cd8c0778eef8b1cbc5c4e7ac70
vn.py <xiaoyou.chen@foxmail.com>
2022-08-21 21:00:45 +0800

    [Mod] 第二段：实现UI行情订阅操作

--- run.py
+++ run.py
@@ -3,6 +3,38 @@ from PySide6 import QtWidgets
 from vnpy_ctp.api import MdApi
 
 
+class SimpleWidget(QtWidgets.QWidget):
+    """简单图形控件"""
+
+    def __init__(self):
+        """构造函数"""
+        super().__init__()      # 这里要首先调用Qt对象C++中的构造函数
+
+        # 用来绑定api对象
+        self.api = None
+
+        # 基础图形控件
+        self.log_monitor = QtWidgets.QTextEdit()
+        self.subscribe_button = QtWidgets.QPushButton("订阅")
+        self.symbol_line = QtWidgets.QLineEdit()
+
+        # 连接按钮函数
+        self.subscribe_button.clicked.connect(self.subscribe_symbol)
+
+        # 设置布局组合
+        vbox = QtWidgets.QVBoxLayout()
+        vbox.addWidget(self.log_monitor)
+        vbox.addWidget(self.symbol_line)
+        vbox.addWidget(self.subscribe_button)
+
+        self.setLayout(vbox)
+
+    def subscribe_symbol(self):
+        """订阅行情"""
+        symbol = self.symbol_line.text()
+        self.api.subscribeMarketData(symbol)
+
+
 class CtpMdApi(MdApi):
     """实现行情API"""
 
@@ -32,9 +64,6 @@ class CtpMdApi(MdApi):
         """用户登录请求回报"""
         if not error["ErrorID"]:
             self.write_log("行情服务器登录成功")
-
-            # 订阅行情推送
-            self.subscribeMarketData("rb2301")
         else:
             self.write_log(f"行情服务器登录失败{error}")
 
@@ -52,12 +81,15 @@ def main():
     # 创建Qt应用
     app = QtWidgets.QApplication()
 
-    # 创建日志监控组件
-    log_monitor = QtWidgets.QTextEdit()
-    log_monitor.show()
+    # 创建图形控件
+    widget = SimpleWidget()
+    widget.show()
 
-    # 创建实例
-    api = CtpMdApi(log_monitor)
+    # 创建API实例
+    api = CtpMdApi(widget.log_monitor)
+
+    # 绑定到图形控件
+    widget.api = api
 
     # 初始化底层
     api.createFtdcMdApi(".")
