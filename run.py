ca6b553da25e6a835bf9664c34c33d804ca0c53d
vn.py <xiaoyou.chen@foxmail.com>
2022-08-22 16:41:21 +0800

    [Mod] 第四段：改为使用CtpGateway和IbGateway获取行情

--- run.py
+++ run.py
@@ -1,18 +1,12 @@
-from threading import Thread
-from datetime import datetime
-
 from PySide6 import QtWidgets
-from ibapi.client import EClient
-from ibapi.wrapper import EWrapper
-from ibapi.common import TickAttrib, TickerId
-from ibapi.ticktype import TickType, TickTypeEnum
-from ibapi.contract import Contract
 
 from vnpy.event import EventEngine, Event
+from vnpy.trader.event import EVENT_LOG, EVENT_TICK
 from vnpy.trader.constant import Exchange
-from vnpy.trader.object import TickData
-from vnpy.trader.event import EVENT_LOG
-from vnpy_ctp.api import MdApi
+from vnpy.trader.object import TickData, LogData, SubscribeRequest
+from vnpy.trader.gateway import BaseGateway
+from vnpy_ctp import CtpGateway
+from vnpy_ib import IbGateway
 
 
 class SimpleWidget(QtWidgets.QWidget):
@@ -23,10 +17,11 @@ class SimpleWidget(QtWidgets.QWidget):
         super().__init__()      # 这里要首先调用Qt对象C++中的构造函数
 
         self.event_engine: EventEngine = event_engine
-        self.event_engine.register(EVENT_LOG, self.update_log)
+        self.event_engine.register(EVENT_LOG, self.process_log_event)
+        self.event_engine.register(EVENT_TICK, self.process_tick_event)
 
         # 用于绑定API对象
-        self.api = None
+        self.gateway: BaseGateway = None
 
         # 基础图形控件
         self.log_monitor: QtWidgets.QTextEdit = QtWidgets.QTextEdit()
@@ -46,201 +41,24 @@ class SimpleWidget(QtWidgets.QWidget):
 
     def subscribe_symbol(self) -> None:
         """订阅行情"""
-        symbol: str = self.symbol_line.text()
+        vt_symbol: str = self.symbol_line.text()
+        symbol, exchange_str = vt_symbol.split(".")
 
-        # self.api.subscribeMarketData(symbol)
+        req = SubscribeRequest(
+            symbol=symbol,
+            exchange=Exchange(exchange_str)
+        )
+        self.gateway.subscribe(req)
 
-        self.api.subscribe(symbol)
-
-    def update_log(self, event: Event) -> None:
+    def process_log_event(self, event: Event) -> None:
         """更新日志"""
-        msg: str = event.data
-        self.log_monitor.append(msg)
+        log: LogData = event.data
+        self.log_monitor.append(log.msg)
 
-
-class CtpMdApi(MdApi):
-    """实现行情API"""
-
-    def __init__(self, event_engine: EventEngine) -> None:
-        """"""
-        super().__init__()
-
-        self.event_engine: EventEngine = event_engine
-
-    def onFrontConnected(self) -> None:
-        """服务器连接成功回报"""
-        self.write_log("行情服务器连接成功")
-
-        # 发起登录操作
-        ctp_req: dict = {
-            "UserID": "000300",
-            "Password": "vnpy1234",
-            "BrokerID": "9999"
-        }
-        self.reqUserLogin(ctp_req, 1)
-
-    def onFrontDisconnected(self, reason: int) -> None:
-        """服务器连接断开回报"""
-        self.write_log(f"行情服务器连接断开{reason}")
-
-    def onRspUserLogin(self, data: dict, error: dict, reqid: int, last: bool) -> None:
-        """用户登录请求回报"""
-        if not error["ErrorID"]:
-            self.write_log("行情服务器登录成功")
-        else:
-            self.write_log(f"行情服务器登录失败{error}")
-
-    def onRtnDepthMarketData(self, data: dict) -> None:
-        """行情数据推送回调"""
-        self.write_log(str(data))
-
-        timestamp: str = f"{data['ActionDay']} {data['UpdateTime']}.{int(data['UpdateMillisec']/100)}"
-        dt: datetime = datetime.strptime(timestamp, "%Y%m%d %H:%M:%S.%f")
-
-        tick: TickData = TickData(
-            symbol=data["InstrumentID"],
-            exchange=Exchange.SHFE,
-            datetime=dt,
-            volume=data["Volume"],
-            turnover=data["Turnover"],
-            open_interest=data["OpenInterest"],
-            last_price=data["LastPrice"],
-            limit_up=data["UpperLimitPrice"],
-            limit_down=data["LowerLimitPrice"],
-            open_price=data["OpenPrice"],
-            high_price=data["HighestPrice"],
-            low_price=data["LowestPrice"],
-            pre_close=data["PreClosePrice"],
-            bid_price_1=data["BidPrice1"],
-            ask_price_1=data["AskPrice1"],
-            bid_volume_1=data["BidVolume1"],
-            ask_volume_1=data["AskVolume1"],
-            gateway_name="CTP"
-        )
-
-        self.write_log(str(tick))
-
-    def write_log(self, msg: str) -> None:
-        """输出日志信息"""
-        event: Event = Event(EVENT_LOG, msg)
-        self.event_engine.put(event)
-
-
-class IbApi(EWrapper):
-    """IB的API实现"""
-
-    TICKFIELD_IB2VT: dict[int, str] = {
-        0: "bid_volume_1",
-        1: "bid_price_1",
-        2: "ask_price_1",
-        3: "ask_volume_1",
-        4: "last_price",
-        5: "last_volume",
-        6: "high_price",
-        7: "low_price",
-        8: "volume",
-        9: "pre_close",
-        14: "open_price",
-    }
-
-    def __init__(self, event_engine: EventEngine):
-        """"""
-        super().__init__()
-
-        self.event_engine: EventEngine = event_engine
-
-        self.client: EClient = EClient(self)
-        self.reqid: int = 0
-
-        self.close = self.client.disconnect
-
-        self.ticks: dict[int, TickData] = {}
-
-    def connectAck(self) -> None:
-        """连接成功回报"""
-        self.write_log("IB TWS连接成功")
-
-    def connectionClosed(self) -> None:
-        """连接断开回报"""
-        self.write_log("IB TWS连接断开")
-
-    def tickPrice(
-        self,
-        reqId: TickerId,
-        tickType: TickType,
-        price: float,
-        attrib: TickAttrib
-    ) -> None:
-        """tick价格更新回报"""
-        super().tickPrice(reqId, tickType, price, attrib)
-
-        tick: TickData = self.ticks[reqId]
-        name: str = self.TICKFIELD_IB2VT.get(tickType, "")
-        if name:
-            setattr(tick, name, price)
-
-        self.write_log(str(tick))
-
-    def tickSize(
-        self,
-        reqId: TickerId,
-        tickType: TickType,
-        size: int
-    ) -> None:
-        """tick数量更新回报"""
-        super().tickSize(reqId, tickType, size)
-
-        tick: TickData = self.ticks[reqId]
-        name: str = self.TICKFIELD_IB2VT.get(tickType, "")
-        if name:
-            setattr(tick, name, size)
-
-        self.write_log(str(tick))
-
-    def tickString(
-        self,
-        reqId: TickerId,
-        tickType: TickType,
-        value: str
-    ) -> None:
-        """tick字符串更新回报"""
-        super().tickString(reqId, tickType, value)
-
-        if tickType == TickTypeEnum.LAST_TIMESTAMP:
-            tick: TickData = self.ticks[reqId]
-            tick.datetime = datetime.fromtimestamp(int(value))
-
-            self.write_log(str(tick))
-
-    def write_log(self, msg: str) -> None:
-        """输出日志信息"""
-        event: Event = Event(EVENT_LOG, msg)
-        self.event_engine.put(event)
-
-    def connect(self, host: str, port: int, clientid: int) -> None:
-        """连接TWS"""
-        self.client.connect(host, port, clientid)
-        self.thread = Thread(target=self.client.run)
-        self.thread.start()
-
-    def subscribe(self, symbol: str) -> None:
-        """订阅美股聚合行情"""
-        ib_contract: Contract = Contract()
-        ib_contract.exchange = "SMART"
-        ib_contract.secType = "CMDTY"
-        ib_contract.currency = "USD"
-        ib_contract.symbol = symbol
-
-        self.reqid += 1
-        self.client.reqMktData(self.reqid, ib_contract, "", False, False, [])
-
-        tick: TickData = TickData(
-            symbol=ib_contract.symbol,
-            exchange=Exchange.SMART,
-            datetime=datetime.now(),
-            gateway_name="ib",
-        )
-        self.ticks[self.reqid] = tick
+    def process_tick_event(self, event: Event) -> None:
+        """更新行情"""
+        tick: TickData = event.data
+        self.log_monitor.append(str(tick))
 
 
 def main():
@@ -256,17 +74,30 @@ def main():
     widget: SimpleWidget = SimpleWidget(event_engine)
     widget.show()
 
-    # CTP API
-    # ctp_api: CtpMdApi = CtpMdApi(event_engine)
-    # ctp_api.createFtdcMdApi(".")
-    # ctp_api.registerFront("tcp://180.168.146.187:10131")
-    # ctp_api.init()
-    # widget.api = ctp_api
+    # CTP交易接口
+    # ctp_setting = {
+    #     "用户名": "000300",
+    #     "密码": "vnpy1234",
+    #     "经纪商代码": "9999",
+    #     "交易服务器": "180.168.146.187:10130",
+    #     "行情服务器": "180.168.146.187:10131",
+    #     "产品名称": "simnow_client_test",
+    #     "授权编码": "0000000000000000"
+    # }
+    # ctp_gateway = CtpGateway(event_engine, "CTP")
+    # ctp_gateway.connect(ctp_setting)
+    # widget.gateway = gateway = ctp_gateway
 
-    # IB API
-    ib_api: IbApi = IbApi(event_engine)
-    ib_api.connect("localhost", 7497, 1)
-    widget.api = ib_api
+    # IB交易接口
+    ib_setting = {
+        "TWS地址": "127.0.0.1",
+        "TWS端口": 7497,
+        "客户号": 1,
+        "交易账户": ""
+    }
+    ib_gateway = IbGateway(event_engine, "IB")
+    ib_gateway.connect(ib_setting)
+    widget.gateway = gateway = ib_gateway
 
     # 启动主线程UI循环
     app.exec()
@@ -274,7 +105,7 @@ def main():
     # 关闭事件引擎
     event_engine.stop()
 
-    ib_api.close()
+    gateway.close()
 
 
 if __name__ == "__main__":
