cf6ed66c793cc5eaf672fcbab9522626a3c2393d
vn.py <xiaoyou.chen@foxmail.com>
2022-08-24 13:21:36 +0800

    [Mod] 第五段：添加策略监控表格

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import json
 from collections import defaultdict
 
 from PySide6 import QtWidgets
@@ -20,6 +21,9 @@ from vnpy_ctp import CtpGateway
 from vnpy_ib import IbGateway
 
 
+EVENT_STRATEGY = "eMyStrategy"
+
+
 class SimpleWidget(QtWidgets.QWidget):
     """简单图形控件"""
 
@@ -39,6 +43,8 @@ class SimpleWidget(QtWidgets.QWidget):
         self.subscribe_button: QtWidgets.QPushButton = QtWidgets.QPushButton("订阅")
         self.symbol_line: QtWidgets.QLineEdit = QtWidgets.QLineEdit()
 
+        self.strategy_monitor: StrategyMonitor = StrategyMonitor(event_engine)
+
         # 连接按钮函数
         self.subscribe_button.clicked.connect(self.subscribe_symbol)
 
@@ -47,6 +53,7 @@ class SimpleWidget(QtWidgets.QWidget):
         vbox.addWidget(self.log_monitor)
         vbox.addWidget(self.symbol_line)
         vbox.addWidget(self.subscribe_button)
+        vbox.addWidget(self.strategy_monitor)
 
         self.setLayout(vbox)
 
@@ -72,8 +79,8 @@ class SimpleWidget(QtWidgets.QWidget):
         self.log_monitor.append(str(tick))
 
 
-class MonitorEngine:
-    """监控引擎"""
+class StrategyEngine:
+    """策略引擎"""
 
     def __init__(self, event_engine: EventEngine, gateway: BaseGateway) -> None:
         """"""
@@ -92,12 +99,17 @@ class MonitorEngine:
         self.event_engine.register(EVENT_LOG, self.process_log_event)
 
         self.tick_history: dict[str, list[TickData]] = defaultdict(list)
-        self.trading_symbols: set[str] = set([
-            "IF2209.CFFEX",
-            "IH2209.CFFEX",
-            "IC2209.CFFEX",
-        ])
+        self.trading_symbols: set[str] = set()
         self.trading_targets: dict[str, int] = defaultdict(int)
+
+        self.load_setting()
+
+    def load_setting(self) -> None:
+        """加载交易代码配置"""
+        with open("setting.json") as f:
+            vt_symbols = json.load(f)
+            for s in vt_symbols:
+                self.trading_symbols.add(s)
 
     def process_tick_event(self, event: Event) -> None:
         """行情事件"""
@@ -215,6 +227,69 @@ class MonitorEngine:
                 self.trading_targets[vt_symbol] = 0
                 print(f"{vt_symbol}卖出平仓1手", tick1.datetime)
 
+        # 推送事件
+        event = Event(
+            type=EVENT_STRATEGY,
+            data={
+                "vt_symbol": vt_symbol,
+                "datetime": tick1.datetime,
+                "last_price": tick1.last_price,
+                "trading_target": self.trading_targets[vt_symbol]
+            }
+        )
+        self.event_engine.put(event)
+
+
+class StrategyMonitor(QtWidgets.QTableWidget):
+    """策略监控表格"""
+
+    def __init__(self, event_engine: EventEngine) -> None:
+        """"""
+        super().__init__()
+
+        self.event_engine: EventEngine = event_engine
+        self.event_engine.register(EVENT_STRATEGY, self.process_strategy_event)
+
+        labels = ["代码", "最新价", "时间", "目标仓位"]
+        self.setHorizontalHeaderLabels(labels)      # 水平表头文字
+        self.verticalHeader().setVisible(False)     # 垂直表头隐藏
+        self.setEditTriggers(self.NoEditTriggers)   # 禁止内容编辑
+
+        self.rows: dict[str, dict[str, QtWidgets.QTableWidgetItem]] = {}
+
+    def process_strategy_event(self, event: Event) -> None:
+        """处理策略事件"""
+        data: dict = event.data
+
+        # 如果是新收到的策略数据，则插入一行
+        if data["vt_symbol"] not in self.rows:
+            self.insertRow(0)
+
+            symbol_cell = QtWidgets.QTableWidgetItem(str(data["vt_symbol"]))
+            datetime_cell = QtWidgets.QTableWidgetItem(str(data["datetime"]))
+            price_cell = QtWidgets.QTableWidgetItem(str(data["last_price"]))
+            target_cell = QtWidgets.QTableWidgetItem(str(data["trading_target"]))
+
+            self.setItem(0, 0, symbol_cell)
+            self.setItem(0, 1, datetime_cell)
+            self.setItem(0, 2, price_cell)
+            self.setItem(0, 3, target_cell)
+
+            self.rows[data["vt_symbol"]] = {
+                "vt_symbol": symbol_cell,
+                "datetime": datetime_cell,
+                "last_price": price_cell,
+                "trading_target": target_cell
+            }
+        # 如果是已有的策略数据，则直接更新
+        else:
+            row = self.rows[data["vt_symbol"]]
+
+            row["vt_symbol"].setText(str(data["vt_symbol"]))
+            row["datetime"].setText(str(data["datetime"]))
+            row["last_price"].setText(str(data["last_price"]))
+            row["trading_target"].setText(str(data["trading_target"]))
+
 
 def main():
     """主函数"""
@@ -223,11 +298,11 @@ def main():
     event_engine.start()
 
     # 创建Qt应用
-    # app: QtWidgets.QApplication = QtWidgets.QApplication()
+    app: QtWidgets.QApplication = QtWidgets.QApplication()
 
     # 创建图形控件
-    # widget: SimpleWidget = SimpleWidget(event_engine)
-    # widget.show()
+    widget: SimpleWidget = SimpleWidget(event_engine)
+    widget.show()
 
     # CTP交易接口
     ctp_setting = {
@@ -242,9 +317,9 @@ def main():
     ctp_gateway = CtpGateway(event_engine, "CTP")
     ctp_gateway.connect(ctp_setting)
     gateway = ctp_gateway
-    # widget.gateway = ctp_gateway
+    widget.gateway = ctp_gateway
 
-    engine: MonitorEngine = MonitorEngine(event_engine, gateway)
+    engine: StrategyEngine = StrategyEngine(event_engine, gateway)
 
     # IB交易接口
     # ib_setting = {
@@ -258,9 +333,9 @@ def main():
     # widget.gateway = gateway = ib_gateway
 
     # 启动主线程UI循环
-    # app.exec()
+    app.exec()
 
-    input()
+    # input()
 
     # 关闭事件引擎
     event_engine.stop()
